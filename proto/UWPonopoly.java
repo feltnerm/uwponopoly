a8b9cc5a75a454e8478cc92d9b3baa96165ee347
Mark Feltner <feltner.mj@gmail.com>
2012-03-16 12:17:25 -0500

    Cleaned up code, added some better comments

--- proto/UWPonopoly.java
+++ proto/UWPonopoly.java
@@ -17,9 +17,17 @@ import java.util.*;
 class UWPonopoly implements Runnable
 {
 
-   // widgets
+   // PRIVATE OBJECTS
+
+   private Thread thread;
+
+   // Game Board
    private GameFrame window;
    private GameBuffer backbuffer;
+   private GamePanel gamepanel;
+   private Property testproperty;
+
+   // Player Stats Panel
    private JPanel dashboard_panel;
    private JPanel dice_panel;
    private JPanel player_stats_panel;
@@ -27,18 +35,11 @@ class UWPonopoly implements Runnable
 
    private JButton sell_button;
 
-
-   private Thread thread;
-
    static final int TICK_LENGTH_MS = 10;
 
-   // desired height and width for the window
+   // Height & Width of the Board
    static final int DESIRED_WIDTH = 550;
    static final int DESIRED_HEIGHT = 350;
-
-   // testing code
-   private GamePanel gamepanel;
-   private Property testproperty;
 
    public static void main(String[] args)
    {
@@ -47,68 +48,84 @@ class UWPonopoly implements Runnable
 
    UWPonopoly()
    {
-      backbuffer = new GameBuffer(DESIRED_WIDTH,DESIRED_HEIGHT, Color.ORANGE);
-      // setup window
+      backbuffer = new GameBuffer(DESIRED_WIDTH, DESIRED_HEIGHT, Color.ORANGE);
+      
+      // GAME WINDOW
       window = new GameFrame("UWPonopoly");
       window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-      window.setSize(DESIRED_WIDTH,DESIRED_HEIGHT);
+      window.setSize(DESIRED_WIDTH, DESIRED_HEIGHT);
       //window.setResizable(false);
       window.setVisible(true);
       //window.setLayout( new BoxLayout(window.getContentPane(), BoxLayout.PAGE_AXIS) );
       window.setLayout( new BorderLayout() );
 
-      // setup gamepanel
+      // GAME PANEL
       gamepanel = new GamePanel( backbuffer );
       //window.getContentPane().add( gamepanel );
 
-      // PROPERTY CONTEXT PANEL - test code
-      // @todo: Separate module
-      // @todo: make it work (?)
-      property_context_panel = new JPanel();
-      property_context_panel.setLayout( new BorderLayout());
-      //property_context_panel.add( new JSeparator(SwingConstants.HORIZONTAL), BorderLayout.NORTH);
-      ImageIcon property_icon = createImageIcon("images/boardwalk.jpg", "deed");
-      property_context_panel.add( new JLabel(property_icon), BorderLayout.NORTH);
-      property_context_panel.add( new JButton("Buy"), BorderLayout.WEST);
-      JButton improve_button = new JButton("Improve");
-      improve_button.setEnabled(false);
-      property_context_panel.add( improve_button, BorderLayout.CENTER);
-      sell_button = new JButton("Sell");
-      sell_button.setEnabled(false);
-      property_context_panel.add( sell_button, BorderLayout.EAST ); 
-
+      // DASHBOARD
       dashboard_panel = new JPanel();
       dashboard_panel.setLayout( new FlowLayout() );
+
+      // Property Context Panel
+      property_context_panel = new JPanel();
+      property_context_panel.setLayout( new BorderLayout());
+
+      ImageIcon property_icon = createImageIcon("images/boardwalk.jpg", "deed");
+      
+      JButton improve_button  = new JButton("Improve");
+      improve_button.setEnabled(false);
+      JButton buy_button      = new JButton("Buy"); 
+      JButton sell_button     = new JButton("Sell");
+      sell_button.setEnabled(false);
+
+      property_context_panel.add( new JLabel(property_icon), BorderLayout.NORTH);
+      property_context_panel.add( buy_button, BorderLayout.WEST);
+      property_context_panel.add( improve_button, BorderLayout.CENTER);
+      property_context_panel.add( sell_button, BorderLayout.EAST );
+      
+      // Dice Control
       dice_panel = new JPanel();
       dice_panel.setLayout( new BorderLayout() );
+      ImageIcon icon = createImageIcon("images/dice.jpg", "dice");
+      dice_panel.add( new JLabel(icon), BorderLayout.NORTH );
+      dice_panel.add( new JButton("Roll!") , BorderLayout.SOUTH);
+
+      // Player Stats
       player_stats_panel = new JPanel();
       player_stats_panel.setLayout( new BorderLayout() );
       player_stats_panel.add( new JLabel("Money: $ 314,159,265"), BorderLayout.NORTH );
       player_stats_panel.add( new JLabel("Current Player: Pat the Pioneer"), BorderLayout.SOUTH );
 
-      // test code
-      testproperty = new Property( backbuffer );
-      /*window.getContentPane().add( testproperty );
-      window.getContentPane().add( new JSeparator(SwingConstants.HORIZONTAL) );
-      window.getContentPane().add( dashboard_panel );*/
-      //window.getContentPane().add( testproperty , BorderLayout.NORTH);
-      ImageIcon board_icon = createImageIcon("images/monopoly-board.png","board");
-      window.getContentPane().add( new JLabel(board_icon), BorderLayout.NORTH );
-      window.getContentPane().add( new JSeparator(SwingConstants.HORIZONTAL), BorderLayout.SOUTH );
-      window.getContentPane().add( dashboard_panel, BorderLayout.SOUTH );
-      //dashboard_panel.add( new JLabel("Current Player: Pat the Pioneer") );
+
+      // Add Panels to Dashboard
       dashboard_panel.add( property_context_panel );
       dashboard_panel.add( player_stats_panel );
       dashboard_panel.add( dice_panel );
-      ImageIcon icon = createImageIcon("images/dice.jpg", "dice");
+
+      // GAME BOARD
+      ImageIcon board_icon = createImageIcon("images/monopoly-board.png","board");
+      window.getContentPane().add( new JLabel(board_icon), BorderLayout.NORTH );
+
+      // Properties
+      testproperty = new Property( backbuffer );
+      /*
+      window.getContentPane().add( testproperty );
+      window.getContentPane().add( new JSeparator(SwingConstants.HORIZONTAL) );
+      window.getContentPane().add( dashboard_panel );
+      */
+      //window.getContentPane().add( testproperty , BorderLayout.NORTH);
+      
+      // Dashboard Panel
+      window.getContentPane().add( new JSeparator(SwingConstants.HORIZONTAL), BorderLayout.SOUTH );
+      window.getContentPane().add( dashboard_panel, BorderLayout.SOUTH );
+      //dashboard_panel.add( new JLabel("Current Player: Pat the Pioneer") );
       //dice_panel.add( new JLabel("Dice", icon, JLabel.CENTER) , BorderLayout.NORTH);
-      dice_panel.add( new JLabel(icon), BorderLayout.NORTH );
-      dice_panel.add( new JButton("Roll!") , BorderLayout.SOUTH);
+
       //window.getContentPane().add( new JProgressBar(5,10) );
 
-
+      // Pack it and Start the Thread
       window.pack();
-
       thread = new Thread(this);
       thread.start();
    }
