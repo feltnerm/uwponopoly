9852dae5461833b82915328581f31caabc4b25d1
Aaron Decker <decker49@gmail.com>
2012-04-01 22:28:32 -0400

    Everything is now written besides roll().

--- tech/dice/dice.java
+++ tech/dice/dice.java
@@ -6,15 +6,26 @@ import java.util.Random;
  */
 class Dice
 {
+   private static int LOW_NUMBER = 1; // lowest number on die
+   private static int HIGH_NUMBER = 6; // highest number on die
    private int dice1, dice2;
-
+   private Random random;
 
    public Dice()
    {
+      random = new Random();
    }
 
    public void roll()
    {
+
+   }
+
+   // formats the dice as a Cartesian coordinate (x,y)
+   @Override
+   public String toString()
+   {
+      return "(" + dice1 + "," + dice2 + ")";
    }
 
    // returns true if the roll is doubles
@@ -23,6 +34,19 @@ class Dice
    // testbed main
    public static void main( String[] args )
    {
-      System.out.println("Dice Testbed.");
+      // declare the test object
+      Dice d = new Dice();
+
+      // print a banner
+      System.out.println("Dice Testbed");
+      System.out.println("============");
+
+      // roll the dice and print the output
+      for( int i = 0; i < 10; i++ )
+      {
+         d.roll();
+         System.out.println( d.toString() + " | " + d.isDoubles() );
+      }
+
    }
 }
