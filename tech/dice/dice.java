6e6531664a5f3020d06d604289ed229d2b119e01
Aaron Decker <decker49@gmail.com>
2012-04-01 22:14:03 -0400

    Added the start of the testbed main().

--- tech/dice/dice.java
+++ tech/dice/dice.java
@@ -1,4 +1,14 @@
 
+/**
+ * Simulates rolling two dice, as used in Monopoly.
+ * This gives it a range of 2-12 for possible values.
+ */
 class Dice
 {
+
+   // testbed main
+   public static void main( String[] args )
+   {
+      System.out.println("Dice Testbed.");
+   }
 }
