0a092e4e71489efe71922a4dc1fcc93de7f39fb8
Mark Feltner <feltner.mj@gmail.com>
2012-04-03 03:20:25 +0000

    Core Config class created with the basic functions needed.

--- tech/configuration/Config.java
+++ tech/configuration/Config.java
@@ -1,6 +1,9 @@
-import java.io.FileInputStream;
+import java.util.Enumeration;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Hashtable;
 
 /**
     Config
@@ -14,40 +17,80 @@ import java.util.Properties;
 
 */
 
-class Config
+class Config extends Properties
 {
 
+    // Path to default configuration file.
     private String CONFIG_PATH = "uwponopoly.conf";
-    Properties config = new Properties();
+    private FileReader config_file;
 
     public Config() {
         // Default constructor
-        loadConfig(CONFIG_PATH);
+        super();
+        this.load(CONFIG_PATH);
     }
 
     public Config(String config_path) {
         // Parameterized constructor for custom configurations
+        super();
         this.CONFIG_PATH = config_path;
-        loadConfig(config_path);
+        this.load(config_path);
     }
 
-    public void loadConfig(String path) {
+    /**
+    public Config(String config_path, Map<String k, String v> config) {
+        super();
+        this.CONFIG_PATH = config_path;
+        this.putAll(config);
+        FileWriter file = new FileWriter(this.CONFIG_PATH);
+        this.store(file, "DERP");
+    }
+    */
+
+    private void load(String path){
         try {
-            this.config.load(new FileInputStream(path));
+            this.config_file = new FileReader(path);
+            super.load(this.config_file);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(path + " not found.");
         }
     }
 
+    public void print() {
+        Enumeration keys = this.keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = this.get(key);
+            System.out.println(key + ":" + value);
+        }
 
+    }
 
     public static void main(String[] args){
 
         // Instantiate our configuration
         Config c = new Config();
-        System.out.println(c.get("DEBUG"));
-        System.out.println(c.get("PLAYERS"));
+        
+        // Print the Configuration
+        c.print();
 
+        // Get values
+        System.out.println("DEBUG: "+c.get("DEBUG"));
+        System.out.println("PLAYERS: "+c.get("PLAYERS"));
+
+        // Declare custom configuration
+        Config c1 = new Config("uwponopoly.conf");
+        c1.print();
+
+        // Error handling
+        Config c2 = new Config("derp");
+        System.out.println("DERP: "+c.get("HERP"));
+
+        // Saving a new config
+        Hashtable opts = new Hashtable();
+        opts.put("DEBUG", "True");
+        opts.put("PLAYERS", "2");
+        Config c3 = new Config("test.conf", opts);
     }
 
 }
