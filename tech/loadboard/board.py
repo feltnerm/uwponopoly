6e84b4bed04ad08228a4afb603f57482404cba9c
Mark Feltner <feltner.mj@gmail.com>
2012-04-12 03:48:49 +0000

    idk what went wrong there, brain tired. going home

--- tech/loadboard/board.py
+++ tech/loadboard/board.py
@@ -8,15 +8,16 @@ space = {'name':'', 'color':'', 'price':'', 'rent':'',
 board = dict()
 
 for x in range(1,41):
-    space['name'] = raw_input('Name: ')
-    space['color'] = raw_input('Color: ')
-    space['price'] = (x * 100) + 50
-    space['rent'] = (x *50)
-    space['1houseprice'] = space['price'] - 50
-    space['2houseprice'] = space['price'] - 25
-    space['3houseprice'] = space['price'] - 20
-    space['4houseprice'] = space['price'] - 15
-    space['hotelprice'] = space['price'] - 10
+    s = space
+    s['name'] = raw_input('Name: ')
+    s['color'] = raw_input('Color: ')
+    s['price'] = (x * 100) + 50
+    s['rent'] = (x * 50)
+    s['1houseprice'] = s['price'] - 50
+    s['2houseprice'] = s['price'] - 25
+    s['3houseprice'] = s['price'] - 20
+    s['4houseprice'] = s['price'] - 15
+    s['hotelprice'] = s['price'] - 10
     pprint(space)
     board[x] = space
 
