06dbb23dab9e9a25b8dc299b8d23f5d4053684d3
Mark Feltner <feltner.mj@gmail.com>
2012-04-24 17:05:50 -0500

    JSON reader work

--- src/board/JSONBoard.java
+++ src/board/JSONBoard.java
@@ -1,48 +1,87 @@
 package board;
 
-import java.io.FileReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import board.Space;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import com.google.gson.reflect.TypeToken;
 
 
 class JSONBoard
 {
 
+	// game /etc, system /etc, ~
     private static String PATH = "../etc/monopoly.json";
+ 	private File JSONBoardFile;
 
     private Gson gson = new Gson();
     private String json;
+    
+    public ArrayList<Space> Spaces;
 
     public JSONBoard()
     {
-       this.json = read();
+    	this(PATH);
     }
-
-    private String read()
+    
+    public JSONBoard(String filepath)
     {
-       try {
-          FileReader fd = new FileReader(PATH);
-          String data = "";
-          while (fd.ready()){
-            data += (char) fd.read();
-          }
-          return (String) data;
-       } catch (IOException e) {
-          System.err.println(e);
-       }
-       return null;
+    	this.PATH = filepath;
+    	File JSONBoardFile = new File(this.PATH);
     }
-
+    
+    public ArrayList<Space> getSpaces(InputStream in) throws IOException
+    {
+    	FileInputStream jsonBoardStream = new FileInputStream(this.JSONBoardFile.)
+    }
+    
+    public ArrayList<Space> readJsonStream(InputStream in) throws IOException {
+    	JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+    	try {
+    		return readSpaceArray(reader);
+    	} finally {
+    		reader.close();
+    	}
+    	
+    }
+    
+    
+    public ArrayList<Space> readSpaceArray(JsonReader reader) throws IOException
+    {
+    	ArrayList<Space> Spaces = new ArrayList<Space>();
+    	
+    	reader.beginArray();
+    	while (reader.hasNext())
+    	{
+    		Spaces.add(readSpace(reader));
+    	}
+    	reader.endArray();
+    	return Spaces;
+    }
+    
+    public ArrayList<Space> getSpaces(String json)
+    {
+    	Type spaceCollectionType = new TypeToken<ArrayList<Space>>(){}.getType();
+    	ArrayList<Space> Spaces = gson.fromJson(json, spaceCollectionType);
+		return Spaces;
+    	
+    }
+    
+    /**
     public ArrayList<Space> getSpaces()
     {
        ArrayList<Space> spaces = gson.fromJson(this.json, new TypeToken<ArrayList<Space>>(){}.getType());
        return spaces;
     }
+    */
 
 }
