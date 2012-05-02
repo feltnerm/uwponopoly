d3607d459748c3bc3a3478434133d911c6b7b0b6
Mark Feltner <feltner.mj@gmail.com>
2012-05-02 13:51:57 -0500

    Menubar improved to handle action events.

--- src/gui/GUIWindow.java
+++ src/gui/GUIWindow.java
@@ -6,24 +6,28 @@ package gui;
 
 // Import Java Packages
 import java.awt.FlowLayout;
+import java.awt.MenuBar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-// Import GUI Mechanics
 import gui.GameFrame;
 
-public class GUIWindow extends GameFrame
+
+public class GUIWindow extends GameFrame implements ActionListener
 {
 	private static String TITLE = "UWPonopoly";
+	private JMenuBar menuBar;
+	private JMenuItem menuItem;
 	
     // Height & Width of the Window
     final int WINDOW_WIDTH = 550;
     final int WINDOW_HEIGHT = 350;
     //final FlowLayout layout = new FlowLayout();
-
-	private JMenuBar menuBar;
 	
 	public GUIWindow()
 	{
@@ -34,14 +38,66 @@ public class GUIWindow extends GameFrame
 		
 		this.menuBar = new JMenuBar();
 			JMenu fileMenu = new JMenu("File");
-			fileMenu.add(new JMenuItem("New"));
-			fileMenu.add(new JMenuItem("Quit"));
+			menuItem = new JMenuItem("New");
+			menuItem.setActionCommand("new");
+			menuItem.addActionListener(this);
+			fileMenu.add(menuItem);
+			
+			menuItem = new JMenuItem("Quit");
+			menuItem.setActionCommand("quit");
+			menuItem.addActionListener(this);
+			fileMenu.add(menuItem);
+			
+			
+			JMenu editMenu = new JMenu("Edit");
+			menuItem = new JMenuItem("Board");
+			menuItem.setActionCommand("edit_board");
+			menuItem.addActionListener(this);
+			editMenu.add(menuItem);
+			
+			menuItem = new JMenuItem("Rules");
+			menuItem.setActionCommand("edit_rules");
+			menuItem.addActionListener(this);
+			editMenu.add(menuItem);
 			
 			JMenu aboutMenu = new JMenu("About");
-			aboutMenu.add(new JMenuItem("Credits"));
-		this.menuBar.add(fileMenu);
+			menuItem.setActionCommand("about");
+			menuItem.addActionListener(this);
+			menuItem = new JMenuItem("Credits");
+			menuItem.setActionCommand("credits");
+			menuItem.addActionListener(this);
+			aboutMenu.add(menuItem);
+		
+			this.menuBar.add(fileMenu);
+		this.menuBar.add(editMenu);
 		this.menuBar.add(aboutMenu);
-        setJMenuBar(menuBar);
+        this.setJMenuBar(menuBar);
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if ("new".equals(e.getActionCommand()))
+		{
+			System.out.print("New");
+		} else if ("quit".equals(e.getActionCommand()))
+		{
+			System.exit(0);
+		} else if ("edit_board".equals(e.getActionCommand()))
+		{
+			System.out.print("Edit Board");
+		} else if ("edit_rules".equals(e.getActionCommand()))
+		{
+			System.out.print("Edit Rules");
+		} else if ("about".equals(e.getActionCommand()))
+		{
+			System.out.print("About");
+		} else if ("credits".equals(e.getActionCommand()))
+		{
+			System.out.print("Credits");
+		} else {
+			
+		}
+		
 	}
 	
 }
